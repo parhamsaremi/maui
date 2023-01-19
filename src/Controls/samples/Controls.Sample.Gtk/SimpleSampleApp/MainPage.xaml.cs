0f8bdaf26604a6bc2295f422f7f9ae7ad378749d
Parham <parhaamsaremi@gmail.com>
2023-01-19 16:51:50 +0330

    Controls.Sample.Gtk: build WelcomePage1

---
+++ src/Controls/samples/Controls.Sample.Gtk/SimpleSampleApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.SimpleSampleApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
