549a5062753918a43e95b591c572da7e0eedf98e
Parham <parhaamsaremi@gmail.com>
2023-04-06 16:59:07 +0330

    WIP: implemented the back button

--- src/Core/src/Platform/Gtk/NavigationView.cs
+++ src/Core/src/Platform/Gtk/NavigationView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Gtk;
 
@@ -7,43 +8,60 @@ namespace Microsoft.Maui.Platform
 
 	public class NavigationView : Gtk.Box
 	{
-		Gtk.Button backButton = new(){Label = "back"};
+		Gtk.Widget? toolbar;
 		Gtk.Widget? pageWidget = null;
-		IMauiContext? mauiContext = null;
+		IMauiContext? MauiContext;
+		Stack<Widget?> stack = new();
 
-		public NavigationView() : base(Gtk.Orientation.Vertical, 0) { }
+		public NavigationView() : base(Gtk.Orientation.Vertical, 0)
+		{
+			var hbox = new Box(Gtk.Orientation.Horizontal, 0);
+			var backBtn = new Button();
+			backBtn.Image = new Image(Stock.GoBack, IconSize.Button);
+			backBtn.Relief = ReliefStyle.None; // Remove button border
+
+			hbox.PackStart(backBtn, false, false, 0);
+			
+			backBtn.Clicked += (o, args) =>
+			{
+				if (stack.Count <= 1) return;
+				var currentPage = stack.Pop();
+				var previousPage = stack.Peek();
+				Remove(currentPage);
+				Add(previousPage);
+				SetChildPacking(previousPage, true, true, 0, PackType.End);
+				pageWidget = previousPage;
+			};
+
+			toolbar = hbox;
+		}
 
 		public void Connect(IStackNavigationView virtualView)
 		{
-			mauiContext = virtualView.Handler?.MauiContext;
+			MauiContext = virtualView.Handler?.MauiContext;
 		}
 
 		public void Disconnect(IStackNavigationView virtualView)
 		{
+			MauiContext = null;
 		}
 
 		public void RequestNavigation(NavigationRequest request)
 		{
 			// stack top is last
 			var page = request.NavigationStack.Last();
-			var newPageWidget = page.ToPlatform(mauiContext!);
-			var oldPageWidget = pageWidget;
+			var newPageWidget = page.ToPlatform(MauiContext!);
+			stack.Push(newPageWidget);
 			if (pageWidget is null)
 			{
-				this.PackStart(backButton, true, false, 0);
-				this.Add(newPageWidget);
+				Add(toolbar);
+				PackEnd(newPageWidget, true, true, 0);
 			}
 			else
 			{
-				backButton.ButtonPressEvent += (o, args) =>
-				{
-					this.Remove(newPageWidget);
-					this.Add(oldPageWidget);
-					pageWidget = oldPageWidget;
-				};
-				this.Remove(pageWidget);
-				this.Add(newPageWidget);
-				// this.SetChildPacking(newPageWidget, true, true, 0, PackType.Start);
+				Remove(pageWidget);
+				Add(newPageWidget);
+				SetChildPacking(newPageWidget, true, true, 0, PackType.End);
 			}
 			pageWidget = newPageWidget;
 		}
