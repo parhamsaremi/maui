11930e3eacfad12e37ce19c36f8f9127a283ff97
Parham Saremi <parhaamsaremi@gmail.com>
2023-05-16 11:50:02 +0430

    Controls.DeviceTests: add AlertDialogTests
    Add tests for checking brightness of AlertDialog button.
    
    Co-authored-by: webwarrior <reg@webwarrior.ws>

---
+++ src/Controls/tests/DeviceTests/Elements/AlertDialog/AlertDialogTests.Android.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.DeviceTests.Stubs;
+using Microsoft.Maui.Platform;
+using Xunit;
+using AndroidX.AppCompat.App;
+using Android.Graphics;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	public partial class AlertDialogTests: ControlsHandlerTestBase
+	{
+
+		async Task<Color> ReturnTextColor()
+		{
+			var mauiContextStub1 = new ContextStub(ApplicationServices);
+			var activity = mauiContextStub1.GetActivity();
+			mauiContextStub1.Context = new Android.Views.ContextThemeWrapper(activity, Resource.Style.Maui_MainTheme_NoActionBar);
+			Color color = Color.Transparent;
+			await InvokeOnMainThreadAsync(() =>
+			{
+				var builder = new AlertDialog.Builder(activity);
+				var alertDialog = builder.Create();
+				alertDialog.Show();
+				var button = alertDialog.GetButton((int)Android.Content.DialogButtonType.Negative);
+				var textColor = button.CurrentTextColor;
+				color = new Color(textColor);
+			});
+
+			return color;
+		}
+
+		[Fact]
+		public void AlertDialogButtonColorLightTheme()
+		{
+			AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightNo;
+			var textColor = ReturnTextColor().Result;
+			Assert.True(textColor.GetBrightness() < 0.5);
+		}
+
+		[Fact]
+		public void AlertDialogButtonColorDarkTheme()
+		{
+			AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightYes;
+			var textColor = ReturnTextColor().Result;
+			Assert.True(textColor.GetBrightness() > 0.5);
+		}
+	}
+}
